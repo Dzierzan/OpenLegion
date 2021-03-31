163e1a74b9d63fe69bd3ad0ea6894d697dfb1d41
Andre Mohren <andremohren@gmail.com>
2021-03-31 08:36:44 +0200

    Added 7th Legion support.

---
+++ OpenRA.Mods.Example/Games/SeventhLegion/SeventhLegion.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using OpenRA.Mods.Example.Games.SeventhLegion.SpriteLoaders;
+
+namespace OpenRA.Mods.Example.Games.SeventhLegion
+{
+    public class SeventhLegion : Game
+    {
+        public SeventhLegion()
+            : base("7LEGION", "7th Legion")
+        {
+        }
+
+        protected override string FindInstallation()
+        {
+            return FindGoGInstallation(1207660773);
+        }
+
+        protected override void UpdateModData(string installPath)
+        {
+            RegisterSpriteLoader<BimLoader>();
+
+            foreach (var directory in Directory.GetDirectories(installPath))
+            {
+                if (Path.GetFileName(directory).Equals("gfx", StringComparison.OrdinalIgnoreCase))
+                    Mount(directory);
+            }
+        }
+    }
+}
